aa12ddb47e5e4f67b85f2b6a9151bb3263fa35db
senumodz-hash <senumodz@gmail.com>
2026-01-13 18:05:33 +0530

    Create page.js

---
+++ app/profile/page.js
@@ -0,0 +1,10 @@
+export default function Profile() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-4">Profile</h1>
+      <p>Name: SENUTH</p>
+      <p>Email: senumodz@gmail.com</p>
+      <p>WhatsApp: +94 743 903 048</p>
+    </>
+  );
+}

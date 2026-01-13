d9bfdbeba21d7f5aa3608adee52adcfdd7daf279
senumodz-hash <senumodz@gmail.com>
2026-01-13 18:04:22 +0530

    Create page.js

---
+++ app/buy/page.js
@@ -0,0 +1,13 @@
+export default function Buy() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-4">Buy Panel</h1>
+      <a
+        href="https://wa.me/94743903048"
+        className="glass px-5 py-3 inline-block font-bold"
+      >
+        Contact on WhatsApp
+      </a>
+    </>
+  );
+}

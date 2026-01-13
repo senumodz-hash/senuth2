983106454234e18339f9123d7e704f68ac495a56
senumodz-hash <senumodz@gmail.com>
2026-01-13 18:02:02 +0530

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Navbar() {
+  return (
+    <header className="glass mx-4 mt-4 p-4 flex justify-end">
+      <span className="text-gray-200">senumodz@gmail.com</span>
+    </header>
+  );
+}
